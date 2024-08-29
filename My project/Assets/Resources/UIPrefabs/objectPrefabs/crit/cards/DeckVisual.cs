9f223c7ca4a105b082f9b09b14c205897c9c3210
Oskar Schermerhorn <hd5808@wayne.edu>
2024-08-29 02:44:14 -0400

    Deck Description
    Clicking deck will show what remaining cards are in the deck

--- My project/Assets/Resources/UIPrefabs/objectPrefabs/crit/cards/DeckVisual.cs
+++ My project/Assets/Resources/UIPrefabs/objectPrefabs/crit/cards/DeckVisual.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class DeckVisual : MonoBehaviour
 {
     CardDealer dealer;
+    [SerializeField] GameObject DeckDescriptionPrefab;
+    [SerializeField] GameObject BlankCardPrefab;
     private void Awake()
     {
         dealer = GameObject.Find("BattleHandler").gameObject.GetComponent<CardDealer>();
@@ -19,5 +21,29 @@ public class DeckVisual : MonoBehaviour
     public void click()
     {
         print("clicked");
+        GameObject Description = Instantiate(DeckDescriptionPrefab, this.transform.parent);
+        for(int i = 0; i < dealer.getDeck().Count; i++)
+        {
+            if (Description.transform.Find(dealer.getDeck()[i].name) != null)
+            {
+                GameObject oldCard = Description.transform.Find(dealer.getDeck()[i].name).gameObject;
+                int quantity = int.Parse(oldCard.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text.Remove(0, 1));
+                quantity++;
+                oldCard.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text = "x" + quantity;
+            }
+            else
+            {
+                GameObject newCard = Instantiate(BlankCardPrefab, Description.transform);
+                for(int j = 0; j< newCard.transform.childCount; j++)
+                {
+                    newCard.transform.GetChild(j).transform.position 
+                        = new Vector2(newCard.transform.GetChild(j).transform.position.x, newCard.transform.GetChild(j).transform.position.y - 7.34f);
+                }
+                newCard.GetComponent<CardDisplay>().setCard(dealer.getDeck()[i]);
+                newCard.name = dealer.getDeck()[i].name;
+                newCard.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text = "x1";
+            }
+            
+        }
     }
 }
